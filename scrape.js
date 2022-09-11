b72654bf09466909f12455c1645bc746cfd623a8
isaiahL88 <isaiah.ib@outlook.com>
2022-09-11 15:07:33 -0400

    method to select comments and put information into object

--- scrape.js
+++ scrape.js
@@ -4,6 +4,9 @@ let pageScraping = false;
 //initializing puppeteer
 async function scrape() {
     if (pageScraping) return;
+    let browser, page;
+    let pageUrl = "https://www.twitch.tv/pengu";
+
     browser = await puppeteer.launch({ headless: true });
     page = await browser.newPage();
     await page.goto(pageUrl, { waitUntil: 'documentloaded', timeout: 60000 });
@@ -12,9 +15,30 @@ async function scrape() {
     //MAY NEED TO CHANGE
     /* wait for chat to be visible */
 
-    async function getComments() {
+    let getComents = await page.evaluate(() => {
         let scrapeComments = [];
+        //All comments from page are Selected
+        //NEED TO: find the correct class Twitch uses
         let comments = document.querySelectorAll('.comment');
 
-    }
+        comments.forEach(comment => {
+            let commentContent = '';
+            //NEED TO: find the correct query to find author and content for twitch
+            let commentAuthor = commment.querySelector('');
+            commentContent = comment.querySelector('');
+
+            //push each comment to array
+            scrapeComments.push({
+                'commentAuthor': commentAuthor,
+                'commentContent': commentContent,
+            });
+        })
+
+        return { 'userComments': scrapeComments }
+
+    });
+
+    //log the comments
+    console.log(await getComments);
+
 }
