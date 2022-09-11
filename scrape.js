ab9d4a45aa3af89b214d82f5d49803a1b646b45f
isaiahL88 <isaiah.ib@outlook.com>
2022-09-11 15:10:57 -0400

    final clause to close browser and wait for next scrape

--- scrape.js
+++ scrape.js
@@ -7,38 +7,50 @@ async function scrape() {
     let browser, page;
     let pageUrl = "https://www.twitch.tv/pengu";
 
-    browser = await puppeteer.launch({ headless: true });
-    page = await browser.newPage();
-    await page.goto(pageUrl, { waitUntil: 'documentloaded', timeout: 60000 });
+    try {
+        pageScraping = true;
+        browser = await puppeteer.launch({ headless: true });
+        page = await browser.newPage();
+        await page.goto(pageUrl, { waitUntil: 'documentloaded', timeout: 60000 });
 
 
-    //MAY NEED TO CHANGE
-    /* wait for chat to be visible */
+        //MAY NEED TO CHANGE
+        /* wait for chat to be visible */
 
-    let getComents = await page.evaluate(() => {
-        let scrapeComments = [];
-        //All comments from page are Selected
-        //NEED TO: find the correct class Twitch uses
-        let comments = document.querySelectorAll('.comment');
+        let getComents = await page.evaluate(() => {
+            let scrapeComments = [];
+            //All comments from page are Selected
+            //NEED TO: find the correct class Twitch uses
+            let comments = document.querySelectorAll('.comment');
 
-        comments.forEach(comment => {
-            let commentContent = '';
-            //NEED TO: find the correct query to find author and content for twitch
-            let commentAuthor = commment.querySelector('');
-            commentContent = comment.querySelector('');
+            comments.forEach(comment => {
+                let commentContent = '';
+                //NEED TO: find the correct query to find author and content for twitch
+                let commentAuthor = commment.querySelector('');
+                commentContent = comment.querySelector('');
 
-            //push each comment to array
-            scrapeComments.push({
-                'commentAuthor': commentAuthor,
-                'commentContent': commentContent,
-            });
-        })
+                //push each comment to array
+                scrapeComments.push({
+                    'commentAuthor': commentAuthor,
+                    'commentContent': commentContent,
+                });
+            })
 
-        return { 'userComments': scrapeComments }
+            return { 'userComments': scrapeComments }
 
-    });
+        });
 
-    //log the comments
-    console.log(await getComments);
+        //log the comments
+        console.log(await getComments);
+    } catch (err) {
+        console.log(err.message);
+    } finally {
+        if (browser) { //cjecl of borwser is open before tring to close
+            await browser.close();
+            console.log('closing browser');
+        }
+        pageScraping = fale;
+        await setTimeout(scrape, 5000); //wait 5 seconds before scraping againr
+    }
 
 }
